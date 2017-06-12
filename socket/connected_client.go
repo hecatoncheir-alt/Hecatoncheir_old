05bef4916d3ee5ca67eb41d97b2f2ce76f8f3125
Vitaliy Vostrikov <rasart.pro@gmail.com>
2017-06-12 21:38:30 +0300

    Adding socket engine

---
+++ socket/connected_client.go
@@ -0,0 +1,56 @@
+package socket
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+)
+
+// ConnectedClient of socket connection
+type ConnectedClient struct {
+	ID           string
+	Channel      chan MessageEvent
+	ClientSocket *websocket.Conn
+	wmu          sync.Mutex
+}
+
+// NewConnectedClient for constructor for ConnectedClient
+func NewConnectedClient(clientConnection *websocket.Conn) *ConnectedClient {
+
+	clientID, _ := uuid.NewUUID()
+	client := ConnectedClient{ID: clientID.String(), ClientSocket: clientConnection, Channel: make(chan MessageEvent)}
+
+	go func() {
+		for {
+
+			inputMessage := MessageEvent{}
+			_, messageBytes, err := clientConnection.ReadMessage()
+
+			if err != nil {
+				fmt.Fprintf(os.Stdout, "Can't receive message from %s. %v \n", client.ID, err)
+				fmt.Fprintf(os.Stdout, "Closed connection of client %s \n", client.ID)
+				close(client.Channel)
+				break
+			}
+
+			json.Unmarshal(messageBytes, &inputMessage)
+
+			inputMessage.ClientID = client.ID
+			client.Channel <- inputMessage
+		}
+	}()
+
+	return &client
+}
+
+// Write need for send event to client
+func (client *ConnectedClient) Write(message string, data interface{}) {
+	event := map[string]interface{}{"Message": message, "Data": data}
+	client.wmu.Lock()
+	websocket.WriteJSON(client.ClientSocket, event)
+	client.wmu.Unlock()
+}
---
+++ socket/engine_test.go
@@ -0,0 +1,65 @@
+package socket
+
+import (
+	"sync"
+	"testing"
+
+	"golang.org/x/net/websocket"
+)
+
+var (
+	once       sync.Once
+	goroutines sync.WaitGroup
+)
+
+func SetUpSocketServer() {
+	engine := NewEngine("v1.0")
+	goroutines.Done()
+	engine.Listen("localhost", 8181)
+	defer engine.Server.Close()
+}
+
+func TestSocketServerCanHandleEvents(test *testing.T) {
+	goroutines.Add(1)
+	go once.Do(SetUpSocketServer)
+	goroutines.Wait()
+
+	socketConnection, err := websocket.Dial("ws://localhost:8181", "", "http://localhost:8181")
+	if err != nil {
+		test.Error(err)
+	}
+
+	inputMessage := make(chan MessageEvent)
+
+	go func() {
+		defer socketConnection.Close()
+		defer close(inputMessage)
+
+		for {
+			messageFromServer := MessageEvent{}
+			err = websocket.JSON.Receive(socketConnection, &messageFromServer)
+
+			if err != nil {
+				test.Error(err)
+				break
+			}
+
+			inputMessage <- messageFromServer
+		}
+	}()
+
+	messageToServer := MessageEvent{Message: "Need api version"}
+	err = websocket.JSON.Send(socketConnection, messageToServer)
+
+	if err != nil {
+		test.Error(err)
+	}
+
+	for messageFromServer := range inputMessage {
+		if messageFromServer.Message != "Version of API" ||
+			messageFromServer.Data.(map[string]interface{})["API version"] != "v1.0" {
+			test.Fail()
+		}
+		break
+	}
+}
