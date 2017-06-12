078735b933b6feee9238d85f20c13de8d0ebd528
Vitaliy Vostrikov <rasart.pro@gmail.com>
2017-06-12 22:37:23 +0300

    Added comments

--- crawler/mvideo/entity_config.go
+++ crawler/mvideo/entity_config.go
@@ -2,12 +2,14 @@ package mvideo
 
 import "github.com/hecatoncheir/Hecatoncheir/crawler"
 
+// ItemConfig structure for parse one item on page
 type ItemConfig struct {
 	ItemSelector        string
 	NameOfItemSelector  string
 	PriceOfItemSelector string
 }
 
+// Page structure for parse parameters from one page
 type Page struct {
 	ItemConfig
 	CityParamPath            string
@@ -17,6 +19,7 @@ type Page struct {
 	PageParamPath            string
 }
 
+// EntityConfig agregate pages structures
 type EntityConfig struct {
 	crawler.Company
 	Pages []Page
